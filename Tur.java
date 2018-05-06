a19857dec17f8acba344a2ba0d2c1a875f4fb5ec
PoNToS96 <32301927+PoNToS96@users.noreply.github.com>
2018-05-06 16:27:50 +0200

    Update Tur.java

--- Tur.java
+++ Tur.java
@@ -12,17 +12,79 @@ public class Tur {
     private int delgArb;
     private int ejAv;
 
-    public Tur(String delgivare, int datum, double start, double stop, double rast, int sok, int sokArb, int delg, int delgArb, int ejAv) {
-        this.delgivare = delgivare;
-        this.datum = datum;
-        this.start = start;
-        this.stop = stop;
-        this.rast = rast;
-        this.sok = sok;
-        this.sokArb = sokArb;
-        this.delg = delg;
-        this.delgArb = delgArb;
-        this.ejAv = ejAv;
+    private Tur(Builder builder) {
+        this.delgivare = builder.delgivare;
+        this.datum = builder.datum;
+        this.start = builder.start;
+        this.stop = builder.stop;
+        this.rast = builder.rast;
+        this.sok = builder.sok;
+        this.sokArb = builder.sokArb;
+        this.delg = builder.delg;
+        this.delgArb = builder.delgArb;
+        this.ejAv = builder.ejAv;
+    }
+
+    public static class Builder {
+        private String delgivare;
+        private int datum;
+        private double start;
+        private double stop;
+        private double rast;
+        private int sok;
+        private int sokArb;
+        private int delg;
+        private int delgArb;
+        private int ejAv;
+
+        public Builder(String delgivare, int datum) {
+            this.delgivare = delgivare;
+            this.datum = datum;
+        }
+
+        public Builder start(double value) {
+            this.start = value;
+            return this;
+        }
+
+        public Builder stop(double value) {
+            this.stop = value;
+            return this;
+        }
+
+        public Builder rast(double value) {
+            this.rast = value;
+            return this;
+        }
+
+        public Builder sok(int value) {
+            this.sok = value;
+            return this;
+        }
+
+        public Builder sokArb(int value) {
+            this.sokArb = value;
+            return this;
+        }
+
+        public Builder delg(int value) {
+            this.delg = value;
+            return this;
+        }
+
+        public Builder delgArb(int value) {
+            this.delgArb = value;
+            return this;
+        }
+
+        public Builder ejAv(int value) {
+            this.ejAv = value;
+            return this;
+        }
+
+        public Tur build() {
+            return new Tur(this);
+        }
     }
 
     public int getDatum() {
@@ -61,12 +123,52 @@ public class Tur {
         return this.ejAv;
     }
 
+    public void setDelgivare(String delgivare) {
+        this.delgivare = delgivare;
+    }
+
+    public void setDatum(int datum) {
+        this.datum = datum;
+    }
+
+    public void setStart(double start) {
+        this.start = start;
+    }
+
+    public void setStop(double stop) {
+        this.stop = stop;
+    }
+
+    public void setRast(double rast) {
+        this.rast = rast;
+    }
+
+    public void setSok(int sok) {
+        this.sok = sok;
+    }
+
+    public void setSokArb(int sokArb) {
+        this.sokArb = sokArb;
+    }
+
+    public void setDelg(int delg) {
+        this.delg = delg;
+    }
+
+    public void setDelgArb(int delgArb) {
+        this.delgArb = delgArb;
+    }
+
+    public void setEjAv(int ejAv) {
+        this.ejAv = ejAv;
+    }
+
     @Override
     public String toString() {
-        return datum + "\ndelgivna: " + delg +
+        return datum + "\nDelgivare: " + delgivare + " Tid: " + start + " - " + stop + " Rast: " + rast + "\ndelgivna: " + delg +
                 ", delgivna på arbetet: " + delgArb +
                 ", sök: " + sok +
                 ", sök på arbetet: " + sokArb +
-                ", ej aviserade: " + ejAv;
+                ", ej aviserade: " + ejAv + "\n";
     }
 }
